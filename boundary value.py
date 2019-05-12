6039a4e3399f44a65294bde07c62d30e835eb013
ABIATHI <47151868+ABIATHI@users.noreply.github.com>
2019-05-12 16:41:00 +0530

    Create boundary value.py

---
+++ boundary value.py
@@ -0,0 +1,11 @@
+mark=59
+if 80<=mark and mark<=100:
+    print("A")
+elif 73<=mark and mark<=79:
+    print("B")
+elif 65<=mark and mark<=72:
+    print("C")
+elif 0<=mark and mark<=64:
+    print("D")
+else:
+    print("Z")
